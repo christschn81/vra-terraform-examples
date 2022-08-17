6b003da46713257756d2322e75e55d8c31a9025f
christschn81 <cschausberge@vmware.com>
2022-08-17 11:44:57 +0200

    second try

--- azure-simple-aks/outputs.tf
+++ azure-simple-aks/outputs.tf
@@ -1,53 +1,23 @@
-output "azurerm_kubernetes_cluster" {
-  description = "Ouputs the Kubernetes cluster attributes"  
-  value = azurerm_kubernetes_cluster.aks
+output "id" {
+  value = azurerm_kubernetes_cluster.example.id
 }
 
-output "azurerm_kubernetes_cluster_kube_config" {
-  description = "Outputs the raw Kubernetes config to be used by kubectl and other compatible tools"  
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
-
-  sensitive = false
+output "kube_config" {
+  value = nonsensitive(azurerm_kubernetes_cluster.example.kube_config_raw)
 }
 
-output "azurerm_kubernetes_cluster_node_pool" {
-  description = "Ouputs the Kubernetes cluster user node pool attributes"  
-  value = azurerm_kubernetes_cluster_node_pool.user_pool
+output "client_key" {
+  value = nonsensitive(azurerm_kubernetes_cluster.example.kube_config.0.client_key)
 }
 
-output "azurerm_container_registry" {
-  description = "Ouputs the container registry attributes"  
-  value = azurerm_container_registry.acr
+output "client_certificate" {
+  value = nonsensitive(azurerm_kubernetes_cluster.example.kube_config.0.client_certificate)
 }
 
-output "azurerm_private_endpoint" {
-  description = "Ouputs the azurerm private endpoint attributes"  
-  value = azurerm_private_endpoint.pep
+output "cluster_ca_certificate" {
+  value = nonsensitive(azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate)
 }
 
-output "azurerm_user_assigned_identity" {
-  description = "Ouputs the user assigned identity attributes"  
-  value = azurerm_user_assigned_identity.id
+output "host" {
+  value = nonsensitive(azurerm_kubernetes_cluster.example.kube_config.0.host)
 }
-
-output "azurerm_log_analytics_workspace" {
-  description = "Ouputs the log analytics workspace attributes"  
-  value = azurerm_log_analytics_workspace.logs
-}
-
-output "azurerm_subnet" {
-  description = "Ouputs the subnet attributes"  
-  value = azurerm_subnet.snet
-}
-
-output "azurerm_subnet_route_table_association" {
-  description = "Ouputs the subnet route table association attributes"  
-  value = azurerm_subnet_route_table_association.aks_subnet_association
-}
-
-
-
-
-
-
-
