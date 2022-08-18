bbbb135772c0cb773313122a09832e92d8f624aa
christschn81 <cschausberge@vmware.com>
2022-08-18 17:10:39 +0200

    updates

--- azure-simple-aks/main.tf
+++ azure-simple-aks/main.tf
@@ -28,28 +28,24 @@ resource "azurerm_kubernetes_cluster" "aks" {
   identity {
     type = "SystemAssigned"
   }
+}
   
-  resource "azurerm_kubernetes_cluster_node_pool" "user_pool" {
-    name                   = "userpool"
-    kubernetes_cluster_id  = azurerm_kubernetes_cluster.aks.id
-    vm_size                = var.user_pool_vm_size
-    vnet_subnet_id         = azurerm_subnet.snet.id
-    enable_auto_scaling    = true
-    max_count              = var.user_pool_max_count
-    min_count              = var.user_pool_min_count
-    enable_node_public_ip  = false
-    zones                  = var.user_pool_availability_zones
+resource "azurerm_kubernetes_cluster_node_pool" "aks" {
+  name                   = "userpool"
+  kubernetes_cluster_id  = azurerm_kubernetes_cluster.aks.id
+  vm_size                = var.user_pool_vm_size
+  enable_auto_scaling    = true
+  max_count              = var.user_pool_max_count
+  min_count              = var.user_pool_min_count
+  enable_node_public_ip  = false
 
-    depends_on = [
-      azurerm_kubernetes_cluster.aks
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      name
     ]
-
-    tags = var.tags
-
-    lifecycle {
-      ignore_changes = [
-        name
-      ]
-    }
   }
 }
